86085acae70063e6a012652e7402a642853db2ff
PedroCaetano-N <pedrohenrique6352006@gmail.com>
2025-02-27 17:32:29 +0000

    NOVO COMMIT

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,17 +5,28 @@ int main(){;
     int nome [50];
     int turisticos;
     float pib;
-    float area;
-    float populaçao;
+    float area, populacao;
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", &nome);
+
+    printf("Digite o número de pontos turíticos: \n");
+    scanf("%d", &turisticos);
+
+    printf("Digite o PIB da cidade: \n");
+    scanf("%f", &pib);
+
+    printf("Digite a área da cidade: \n");
+    scanf("%f", &area);
+
+    printf("Digite a população da cidade: \n");
+    scanf("%f", &populacao);
+
+    printf("Nome da cidade: %s - Número de pontos turísticos: %d\n", nome, turisticos);
+    printf("PIB da cidade: %f - Área da cidade: %f - Quantidade de pessoas na cidade: %f\n", pib, area, populacao);
+
+    return 0;
     
-
-
-
-
-
-
-
-
 
 
 }
